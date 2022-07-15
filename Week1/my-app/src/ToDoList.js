196f684e5780a631e17ff715eb17b275d666f303
Green1-j <joegr193@gmail.com>
2022-07-15 11:40:12 -0400

    week1

---
+++ Week1/my-app/src/ToDoList.js
@@ -0,0 +1,26 @@
+
+
+
+
+function ToDoList() {
+
+    return  ( 
+        <div>
+        <h1>List of Todos</h1>
+        <input id="Todo1"  type="checkbox" />
+        <span style={{fontWeight:' bold'}}>go to Gym</span><br></br>
+
+        <input id="Todo2"  type="checkbox" />
+        <span style={{fontWeight:' bold'}}>do laundry</span><br></br>
+
+        <input id="Todo3"  type="checkbox" />
+        <span style={{fontWeight:' bold'}}>food shopping</span><br></br>
+
+        <input id="Todo4"  type="checkbox" />
+        <span style= {{fontWeight:' bold'}}>do homework</span><br></br>
+
+        </div>
+    );
+}
+
+export default ToDoList;
